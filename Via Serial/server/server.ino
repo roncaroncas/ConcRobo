2fcd69e61e2fff89a5e231e210863c1baf495f88
Victor Roncalli <victor.roncalli.souza@gmail.com>
2017-05-13 01:33:56 -0300

    Arduino now sets the right relays logic instead of setting one pin at a time

--- Via Serial/server/server.ino
+++ Via Serial/server/server.ino
@@ -1,5 +1,5 @@
 //TODO:
-//Atualizar protocolo de: Pino-Set-High para Funcao-Timeleft
+//Atualizar protocolo de: Pino-Set-High para Funcao-Set
 //Atualizar comunicacao de: Serial para Ethernet 
 //Atualizar de programação simples para protomultithread para atuar paralelamente: Read signal e Varredura de funcoes
 //Colocar variaveis de controle de funcoes para tempo (pesquisar sobre "millis()")
@@ -11,13 +11,19 @@ int msgSize = 3;
 char msg[5];
 String message;
 int pin;
+int dir;
 boolean setter; //true: setter, false: getter
 boolean high; //true: HIGH, false: LOW
 
-// funcoes de varredura
-float func0tl = 0; //time left for function 1 to work
-float func1tl = 0; //...
-float func2tl = 0;
+boolean relA = false; // true=Vcc, false=Gnd
+boolean relB = false;
+boolean relC = false;
+boolean relD = false;
+
+int pinRelA = 4;
+int pinRelB = 5;
+int pinRelC = 6;
+int pinRelD = 7;
 
 void setup() {
   Serial.begin(9600);
@@ -25,20 +31,18 @@ void setup() {
   //TODO: settar corretamente os pinos por funcao
 
   //Setando todos os pinos como output
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
+  
+  //Pinos do relé (10, 11, 12, 13)
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
+
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
+  
 
   delay(200); //espera 0,2 segundos para estabilizar conexao 
 }
@@ -53,20 +57,16 @@ void loop() {
     applyMsg(); // msg -> message, pin, setter, high
 
     exeMsg(); // atualiza os pinos de acordo com msg
-
+  
     sendResponse(); //print response in Serial.println
   }
 
-  //---------EXECUÇÃO-----------
-  varrerFuncoes(); //loop em cima de comandos (TODO)
-  
 }
 
 //------ FUNCOES DE COMUNICACAO -----
 
 void readSerial() {  
   String endChar = "\n";
-  //int t = 0;
   //resetting i and msg
   for (i = 0; i < msgSize; i++) {
     msg[i] = (char)0;
@@ -95,60 +95,151 @@ void applyMsg(){
   }
   
   //atribui os valores de cada caracter de "msg" para as devidas variáveis"
+
   pin = charToPin(msg[0]);  //usa a funcao charToPin que converte um char para um Pino em int
+  dir = (msg[0] - '0'); // int
   setter = (msg[1] - '0'); //boolean
   high = (msg[2] - '0'); //boolean
+
 }
 
 void sendResponse(){
   
   //retorna uma mensagem no formato "--MESSAGE 311   PIN: 3   SETTER: 1   HIGH: 1"
   String response;
-  response = "--MESSAGE: " + message + "   PIN: " + pin + "   SETTER: " + setter + "   HIGH: " + high;
+  //response = "--MESSAGE: " + message + "   PIN: " + pin + "   SETTER: " + setter + "   HIGH: " + high;
+  //response = "--MESSAGE: " + message + "   DIR: " + dir + "   SETTER: " + setter + "   HIGH: " + high;
+  response = "--MESSAGE: " + message + "   relA: " + relA +   "   relB: " + relB + "   relC: " + relC + "    relD:   " + relD;
   Serial.println(response);
 }
 
 void exeMsg(){
   
   //Atualiza de fato os valores dos pinos
-  if (setter == true) {
-    if (high == true) {
-      digitalWrite(pin, HIGH);
-    } else {
-      digitalWrite(pin, LOW);
-    }
-  }
-  
-}
+  //if (setter == true) {
+  //  if (high == true) {
+  //    digitalWrite(pin, HIGH);bata
+  //  } else {
+  //    digitalWrite(pin, LOW);
+  //  }
 
-// -------- VARREDURA FUNCTIONS -------
-
-void varrerFuncoes(){
-  if (func0tl>0){
-    function0();
-  }
-  if (func1tl>0){
-    function1();
-  }
-  if (func2tl>0){
-    function2();
+  if (setter == true){
+    dirToReles();  
+    setReles();
   }
 }
 
-//-ABORT FUNCTION
-void function0(){
+
+// -------- SETTER FUNCTIONS -------
+
+void dirToReles(){
+switch (dir){
+    
+    //UP
+    case 0 :
+      relA = true;
+      relB = false;
+      relC = true;
+      relD = false;
+      break;
+
+    //UP_RIGHT
+    case 1 :
+      relA = true;
+      relB = false;
+      relC = false;
+      relD = false;
+      break;
+      
+    //RIGHT
+    case 2 :
+      relA = false;
+      relB = true;
+      relC = true;
+      relD = false;
+      break;
+    
+    //DOWN_RIGHT
+    case 3 :
+      relA = false;
+      relB = false;
+      relC = false;
+      relD = true;
+      break;
+      
+    //DOWN
+    case 4 :
+      relA = false;
+      relB = true;
+      relC = false;
+      relD = true;
+      break;
+      
+    //DOWN_LEFT
+    case 5 :
+      relA = false;
+      relB = true;
+      relC = false;
+      relD = false;
+      break;
+      
+    //LEFT
+    case 6 :
+      relA = true;
+      relB = false;
+      relC = false;
+      relD = true;
+      break;
+    
+    //UP_LEFT
+    case 7 :
+      relA = false;
+      relB = false;
+      relC = true;
+      relD = false;
+      break;
+      
+    //STOP
+    case 8:
+      relA = false;
+      relB = false;
+      relC = false;
+      relD = false;
+      break;
   
+    default:
+      Serial.println("nao entrou em nenhum case");
+  }
 }
 
-void function1(){
+void setReles(){
   
-}
-
-void function2(){
+  //TEMP
+  if (relA == true){
+   digitalWrite(pinRelA, HIGH); 
+  } else {
+    digitalWrite(pinRelA, LOW);
+  }
   
+  if (relB == true){
+   digitalWrite(pinRelB, HIGH); 
+  } else {
+    digitalWrite(pinRelB, LOW);
+  }
+  
+  if (relC == true){
+   digitalWrite(pinRelC, HIGH); 
+  } else {
+    digitalWrite(pinRelC, LOW);
+  }
+
+  if (relD == true){
+   digitalWrite(pinRelD, HIGH); 
+  } else {
+    digitalWrite(pinRelD, LOW);
+  }
+    
 }
-
-
 
 //UTILS FUNCTIONS
 
